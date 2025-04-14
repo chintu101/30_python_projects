0ba33e8f34323d0d1db5e41a1e9571e0c1fe1a7d
Sarvesh Sathyanarayanan <sarvesh112170@gmail.com>
2025-04-14 10:25:47 +0530

    adding structure

--- alarm.py
+++
@@ -1,7 +0,0 @@
-import time
-
-print(time.strftime("Current time: %H:%M"))
-alarm_time = input("Enter the alarm time (HH:MM): ")
-while time.strftime("%H:%M") != alarm_time:
-    time.sleep(1)
-print("Time to wake up!")
